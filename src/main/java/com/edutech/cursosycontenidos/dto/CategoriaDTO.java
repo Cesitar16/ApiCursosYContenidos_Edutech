6f278bba79392f8919734dfc102356f39593fac3
LucasMoncadaCL <lucasmoncada017@gmail.com>
2025-07-02 22:31:28 -0400

    Aplicado Metodo de HATEOAS

--- src/main/java/com/edutech/cursosycontenidos/dto/CategoriaDTO.java
+++ src/main/java/com/edutech/cursosycontenidos/dto/CategoriaDTO.java
@@ -3,11 +3,12 @@ package com.edutech.cursosycontenidos.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CategoriaDTO {
+public class CategoriaDTO extends RepresentationModel<CategoriaDTO> {
     private Integer idCategoria;
     private String nombreCate;
 }
